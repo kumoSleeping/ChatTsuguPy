e7f0d0cbe14f91848676d38dd7f3589e69828f84
kumoSleeping <zjr2992@outlook.com>
2024-04-12 15:10:36 +0800

    0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='tsugu',
-    version='0.3.3',
+    version='0.3.4',
     author='kumoSleeping',
     author_email='zjr2992@outlook.com',
     license="MIT",
