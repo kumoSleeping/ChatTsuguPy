cd1f2576be4e7f1f3db38c54bfaf2c8b177c267f
kumoSleeping <zjr2992@outlook.com>
2024-05-21 15:50:51 +0800

    1.1.0-rc4 发rc版：一坨更改 修了一坨bug，比如config失效，help问题

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='tsugu',
-    version='1.1.0-rc3',
+    version='1.1.0-rc4',
     author='kumoSleeping',
     author_email='zjr2992@outlook.com',
     license="MIT",
