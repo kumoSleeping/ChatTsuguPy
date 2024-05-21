166a5a977258456c1cdf41cecb71b427f036ca50
kumoSleeping <zjr2992@outlook.com>
2024-05-22 00:22:12 +0800

    2.0.4 fix: 绑定玩家搞成解除绑定

--- tsugu/src/remote/bind_player.py
+++ tsugu/src/remote/bind_player.py
@@ -20,7 +20,8 @@ def handler(message: str, user_id: str, platform: str, channel_id: str):
 
     if res.matched:
         user = get_user(user_id, platform)
-        r = tsugu_api.bind_player_request(platform, user.user_id, user.server_mode, False)
+        r = tsugu_api.bind_player_request(platform, user_id, user.server_mode, True)
+        print(r)
         if r.get('status') != 'success':
             return text_response(r.get('data'))
         return text_response(
