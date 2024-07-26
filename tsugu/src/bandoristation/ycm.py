3ed81ab2132d25820926c57a634868d288f22be7
kumoSleeping <zjr2992@outlook.com>
2024-07-26 16:20:16 +0800

    3.1.3 fix ycm bug **2

--- tsugu/src/bandoristation/ycm.py
+++ tsugu/src/bandoristation/ycm.py
@@ -1,8 +1,11 @@
+from tsugu_api_core._typing import _Room
+
 from ...utils import text_response, User, server_id_2_server_name, server_name_2_server_id
 import tsugu_api
 import tsugu_api_async
 from arclet.alconna import Alconna, Option, Subcommand, Args, CommandMeta, Empty, Namespace, namespace, command_manager
 from tsugu.utils import get_user, get_user_async
+from typing import Optional
 
 alc = Alconna(
         ["ycm", "车来", "有车吗"],
@@ -24,22 +27,36 @@ def handler(message: str, user_id: str, platform: str):
         if not data:
             return text_response('myc')
 
-        new_data = {}
         user = get_user(user_id, platform)
+        new_data_list = []
+
         for i in data:
-            if len(user.user_player_list) >= user.user_player_index + 1:  # 只添加有玩家数据的信息
-                new_data.update({'player': {'playerId': user.user_player_list[user.user_player_index]['playerId'],
-                                            'server': user.user_player_list[user.user_player_index]['server']}})
-            new_data.update({'number': int(i['number'])})
-            new_data.update({'source': i['source_info']['name']})
-            new_data.update({'userId': i['user_info']['user_id']})
-            new_data.update({'time': i['time']})
-            new_data.update({'avatarUrl': 'https://asset.bandoristation.com/images/user-avatar/' + i['user_info']['avatar']})
-            new_data.update({'userName': i['user_info']['username']})
-            new_data.update({'rawMessage': i['raw_message']})
-        # 过滤掉 number 相同的
+            new_data_list.append({})
+
+            # 检查是否有足够的玩家信息
+            if len(user.user_player_list) > user.user_player_index:
+                # 添加玩家信息
+                new_data_list[-1].update({
+                    'playerId': user.user_player_list[user.user_player_index]['playerId'],
+                    'server': user.user_player_list[user.user_player_index]['server']
+                })
+
+            # 更新其他数据
+            new_data_list[-1].update({'number': int(i['number'])})
+            new_data_list[-1].update({'source': i['source_info']['name']})
+            new_data_list[-1].update({'userId': i['user_info']['user_id']})
+            new_data_list[-1].update({'time': i['time']})
+
+            if i['user_info']['avatar']:
+                new_data_list[-1].update({'avatarUrl': 'https://asset.bandoristation.com/images/user-avatar/' + i['user_info']['avatar']})
+            elif i['user_info']['type'] == 'qq':
+                new_data_list[-1].update({'avatarUrl': f'https://q2.qlogo.cn/headimg_dl?dst_uin={i["user_info"]["user_id"]}&spec=100'})
+            new_data_list[-1].update({'userName': i['user_info']['username']})
+            new_data_list[-1].update({'rawMessage': i['raw_message']})
+
         try:
-            return tsugu_api.room_list([new_data])
+            new_data_list.reverse()
+            return tsugu_api.room_list(new_data_list)  # type: ignore
         except Exception as e:
             return text_response(e)
 
@@ -57,22 +74,39 @@ async def handler_async(message: str, user_id: str, platform: str):
         if not data:
             return text_response('myc')
 
-        new_data = {}
         user = await get_user_async(user_id, platform)
+        new_data_list = []
+
         for i in data:
-            if len(user.user_player_list) >= user.user_player_index + 1:  # 只添加有玩家数据的信息
-                new_data.update({'player': {'playerId': user.user_player_list[user.user_player_index]['playerId'], 'server': user.user_player_list[user.user_player_index]['server']}})
-            new_data.update({'number': int(i['number'])})
-            new_data.update({'source': i['source_info']['name']})
-            new_data.update({'userId': i['user_info']['user_id']})
-            new_data.update({'time': i['time']})
-            new_data.update({'avatarUrl': 'https://asset.bandoristation.com/images/user-avatar/' + i['user_info']['avatar']})
-            new_data.update({'userName': i['user_info']['username']})
-            new_data.update({'rawMessage': i['raw_message']})
-        # 过滤掉 number 相同的
+            new_data_list.append({})
+
+            # 检查是否有足够的玩家信息
+            if len(user.user_player_list) > user.user_player_index:
+                # 添加玩家信息
+                new_data_list[-1].update({
+                    'playerId': user.user_player_list[user.user_player_index]['playerId'],
+                    'server': user.user_player_list[user.user_player_index]['server']
+                })
+
+            # 更新其他数据
+            new_data_list[-1].update({'number': int(i['number'])})
+            new_data_list[-1].update({'source': i['source_info']['name']})
+            new_data_list[-1].update({'userId': i['user_info']['user_id']})
+            new_data_list[-1].update({'time': i['time']})
+
+            if i['user_info']['avatar']:
+                new_data_list[-1].update({'avatarUrl': 'https://asset.bandoristation.com/images/user-avatar/' + i['user_info']['avatar']})
+            elif i['user_info']['type'] == 'qq':
+                new_data_list[-1].update({'avatarUrl': f'https://q2.qlogo.cn/headimg_dl?dst_uin={i["user_info"]["user_id"]}&spec=100'})
+            new_data_list[-1].update({'userName': i['user_info']['username']})
+            new_data_list[-1].update({'rawMessage': i['raw_message']})
+
         try:
-            return await tsugu_api_async.room_list([new_data])
+            new_data_list.reverse()
+            return await tsugu_api_async.room_list(new_data_list)  # type: ignore
         except Exception as e:
             return text_response(e)
 
     return res
+
+
